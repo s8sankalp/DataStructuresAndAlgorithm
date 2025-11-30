f9edce21f7a281771944ec37b738d5e955306e1d
sankalp suman <sankalpsuman08@gmail.com>
2025-11-30 15:47:37 +0530

    unique

---
+++ unique.java
@@ -0,0 +1,24 @@
+public class unique {
+     public int uniquePaths(int m, int n) {
+         int dp[][] = new int[m][n];
+    dp[0][0]=1;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==0&&j==0)continue;
+            int up=i>0?dp[i-1][j]:0;
+            int left=j>0?dp[i][j-1]:0;
+            dp[i][j]=up+left;
+        }
+    }
+    return dp[m-1][n-1];
+}
+    public static void main(String[] args){
+        unique u=new unique();
+        int m=3;
+        int n=7;
+        int result=u.uniquePaths(m,n);
+        System.out.println(result);
+    }
+}
