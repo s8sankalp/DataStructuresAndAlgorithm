23270e8530e6574018d08520e8e77bb572dc498a
sankalp suman <sankalpsuman08@gmail.com>
2026-02-21 18:25:46 +0530

    Add dtob.java

---
+++ dtob.java
@@ -0,0 +1,9 @@
+public class dtob {
+    public static void main(String[] args)
+    {
+        int n=2;
+        String binary=Integer.toBinaryString(n);
+        System.out.println(binary);
+    }
+    
+}
