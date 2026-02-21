709a6d3faa40883f07594b86a69109421ccd3922
sankalp suman <sankalpsuman08@gmail.com>
2026-02-21 18:24:04 +0530

    test

---
+++ test1.java
@@ -0,0 +1,15 @@
+public class test1 {
+    public static void main(String [] args)
+    {
+        
+        Integer a = 100;
+Integer b = 100;
+System.out.println(a == b);  // true
+
+Integer x = 200;
+Integer y = 200;
+System.out.println(x == y);  // false
+
+    }
+    
+}
