5cdba7ea7b895345a856f68a1c678239d43f48d4
sankalp suman <sankalpsuman08@gmail.com>
2026-03-22 14:46:23 +0530

    parity array 1

---
+++ parity_arr1.java
@@ -0,0 +1,10 @@
+public class parity_arr1 {
+    public boolean parity(int[] arr)
+  {  return true;}
+    public static void main(String[] args)
+    {
+        parity_arr1 sol=new parity_arr1();
+        int arr[]={2,3};
+        System.out.println(sol.parity(arr));
+    }
+}
