de6a3f7555af79151e542c43376905ae6e4cc71e
sankalp suman <sankalpsuman08@gmail.com>
2026-06-26 13:16:42 +0530

    Time: 78 ms (86.83%), Space: 72 MB (34.88%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    Map<Integer, List<Integer>> map;
+    Random random;
+
+    public Solution(int[] nums) {
+        map = new HashMap<>();
+        random = new Random();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> indices = map.get(target);
+        return indices.get(random.nextInt(indices.size()));
+    }
+}
