d4a886f18881a6cc978f1f395fd3c05772b2de22
sankalp suman <sankalpsuman08@gmail.com>
2026-06-29 00:00:35 +0530

    Time: 11 ms (98.03%), Space: 54.3 MB (19.44%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        int n = intervals.length;
+
+        int[][] starts = new int[n][2];
+
+        // store start value and original index
+        for (int i = 0; i < n; i++) {
+            starts[i][0] = intervals[i][0];
+            starts[i][1] = i;
+        }
+
+        Arrays.sort(starts, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int target = intervals[i][1];
+
+            int left = 0, right = n - 1;
+            int idx = -1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (starts[mid][0] >= target) {
+                    idx = starts[mid][1];
+                    right = mid - 1; // search for smaller valid start
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            ans[i] = idx;
+        }
+
+        return ans;
+    }
+}
