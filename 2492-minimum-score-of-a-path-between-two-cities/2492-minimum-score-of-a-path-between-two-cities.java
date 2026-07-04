7667ce042c9517d049d8848ff701d3e69f8c8400
sankalp suman <sankalpsuman08@gmail.com>
2026-07-05 00:09:41 +0530

    Time: 29 ms (54.17%), Space: 162.5 MB (60.96%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,23 +1,45 @@
 class Solution {
-    int find(int[] root, int i) {
-        if (root[i] == i)
-            return i;
-        return root[i] = find(root, root[i]);
-    }
-
     public int minScore(int n, int[][] roads) {
-        int[] root = new int[n + 1];
-        for (int i = 0; i <= n; i++)
-            root[i] = i;
 
-        for (int[] r : roads)
-            root[find(root, r[0])] = find(root, r[1]);
+        List<List<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
 
-        int res = 10001;
-        for (int[] r : roads)
-            if (find(root, r[0]) == find(root, 1))
-                res = Math.min(res, r[2]);
+        for (int[] road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int w = road[2];
 
-        return res;
+            adj.get(u).add(new int[]{v, w});
+            adj.get(v).add(new int[]{u, w});
+        }
+
+        boolean[] vis = new boolean[n + 1];
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(1);
+        vis[1] = true;
+
+        int ans = Integer.MAX_VALUE;
+
+        while (!q.isEmpty()) {
+
+            int node = q.poll();
+
+            for (int[] edge : adj.get(node)) {
+                int nei = edge[0];
+                int wt = edge[1];
+
+                ans = Math.min(ans, wt);
+
+                if (!vis[nei]) {
+                    vis[nei] = true;
+                    q.offer(nei);
+                }
+            }
+        }
+
+        return ans;
     }
 }
