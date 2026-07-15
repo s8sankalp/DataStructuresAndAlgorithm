210d332cad89a67cb7373334d754c8766f6101e5
sankalp suman <sankalpsuman08@gmail.com>
2026-07-15 23:36:22 +0530

    Time: 9 ms (18.96%), Space: 59.6 MB (12.21%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+
+        int n = distance.length;
+
+        for (int i = 3; i < n; i++) {
+
+            // Case 1
+            if (distance[i] >= distance[i - 2] &&
+                distance[i - 1] <= distance[i - 3]) {
+                return true;
+            }
+
+            // Case 2
+            if (i >= 4 &&
+                distance[i - 1] == distance[i - 3] &&
+                distance[i] + distance[i - 4] >= distance[i - 2]) {
+                return true;
+            }
+
+            // Case 3
+            if (i >= 5 &&
+                distance[i - 2] >= distance[i - 4] &&
+                distance[i] + distance[i - 4] >= distance[i - 2] &&
+                distance[i - 1] <= distance[i - 3] &&
+                distance[i - 1] + distance[i - 5] >= distance[i - 3]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
